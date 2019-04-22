97568916cc18ef3dc8aeef5fc3a5213d7028d90d
Jon <roomcleaner@protonmail.com>
2019-04-22 00:26:45 -0500

    Basic Site Layout with bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const fixHeader = target => {
+//     const io = new IntersectionObserver((entries, observer) => {
+//         let wrap = document.querySelector('#wrap');
+//         entries.forEach(entry => {
+//             if (entry.intersectionRatio <= 0) {
+//                 wrap.classList.add('fix-search');
+//             } else if (entry.intersectionRatio > 0) {
+//                 wrap.classList.remove('fix-search');
+//             }
+//         });
+//     });
+//     io.observe(target);
+// };
+
+// fixHeader(document.querySelector('div.hero'));
